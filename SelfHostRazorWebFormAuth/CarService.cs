5edefc184a695a6b22b8ce2ce479ae224f8b076d
lderache <laurent.derache@gmail.com>
2014-05-05 00:15:09 +0800

    first commit

---
+++ SelfHostRazorWebFormAuth/CarService.cs
@@ -0,0 +1,43 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostRazorWebFormAuth
+{
+    // Basic car class
+    public class Car
+    {
+        public string Plate { get; set; }
+    }
+
+    // Request DTO
+    [Route("/Cars", "GET")]
+    public class CarRequest
+    {
+    }
+
+    // Response DTO
+    public class CarResponse
+    {
+        public List<Car> Cars { get; set; }
+    }
+
+    [Authenticate]
+    [DefaultView("Cars")]
+    public class CarService : Service
+    {
+        List<Car> CarsResult = new List<Car>
+        {
+            new Car { Plate = "FG98745" },
+            new Car { Plate = "VN236PL" }
+        };
+
+        public object Get(CarRequest request)
+        {
+            return new CarResponse { Cars = CarsResult };
+        }
+    }
+}
---
+++ SelfHostRazorWebFormAuth/Program.cs
@@ -0,0 +1,60 @@
+﻿using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Logging;
+using ServiceStack.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostRazorWebFormAuth
+{
+    class Program
+    {
+        //Define the Web Services AppHost
+        public class AppHost : AppHostHttpListenerBase
+        {
+            public AppHost()
+                : base("HttpListener SelfHost Demo", typeof(CarService).Assembly) { }
+
+            public override void Configure(Funq.Container container)
+            {
+                Plugins.Add(new RazorFormat());
+
+                Plugins.Add(new AuthFeature(() => new AuthUserSession(),
+                  new IAuthProvider[] { 
+                    new CredentialsAuthProvider(), //HTML Form post of UserName/Password credentials
+                  }));
+
+
+                var userRep = new InMemoryAuthRepository();
+                container.Register<IUserAuthRepository>(userRep);
+
+                UserAuth userDemo = new UserAuth
+                {
+                    UserName = "demo"
+                };
+
+                userRep.CreateUserAuth(userDemo, "demo");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LogManager.LogFactory = new ConsoleLogFactory();
+
+            var listeningOn = args.Length == 0 ? "http://*:8090/" : args[0];
+
+            var appHost = new AppHost();
+
+            appHost.Init();
+            appHost.Start(listeningOn);
+
+            Console.WriteLine("AppHost Created at {0}, listening on {1}",
+                DateTime.Now, listeningOn);
+
+            Console.ReadKey();
+        }
+    }
+}
